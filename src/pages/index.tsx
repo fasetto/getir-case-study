2d0c6797e5b9b67acb3d220bd68cddc2d3c29ab5
Serkan Bircan <serkanbircan21@yandex.com>
2021-11-06 19:25:15 +0300

    Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,7 @@
-import Head from "next/head";
+import styled from "styled-components";
+
+import { Header } from "@/components";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Next JS Template</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center flex-1 mt-8 px-20 text-center">
-        <h1 className="text-5xl font-bold">
-          Welcome to{" "}
-          <a
-            className="text-blue-600 hover:underline"
-            href="https://nextjs.org"
-          >
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  );
+  return <Header />;
 }
