76da29258775f145f9d939bf74d4a1608d07e47e
Serkan Bircan <serkanbircan21@yandex.com>
2021-11-06 19:23:18 +0300

    Add theme variables

---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+export const breakpoints = {
+  sm: "(min-width: 640px)",
+  md: "(min-width: 768px)",
+  lg: "(min-width: 1024px)",
+  xl: "(min-width: 1280px)",
+};
+
+export const colors = {
+  primary: "#1EA4CE",
+  primaryDark: "#147594",
+  gray50: "#F4F4F4",
+  gray300: "#A8A8A8",
+  gray500: "#6F6F6F",
+  gray700: "#191919",
+};
