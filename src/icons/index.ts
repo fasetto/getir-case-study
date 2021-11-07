efb32a6a1373188f8ec28c31916ff7fc735b84d3
Serkan Bircan <serkanbircan21@yandex.com>
2021-11-07 17:48:15 +0300

    Add more icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -5,3 +5,4 @@ export { default as ChevronLeft } from "./ChevronLeft";
 export { default as ChevronRight } from "./ChevronRight";
 export { default as MinusIcon } from "./MinusIcon";
 export { default as PlusIcon } from "./PlusIcon";
+export { default as DotsHorizontal } from "./DotsHorizontal";
