19c9317f677437f5010806cd81b0bbbbde7be50a
Serkan Bircan <serkanbircan21@yandex.com>
2021-11-07 12:13:24 +0300

    Add fetcher utility

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,34 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+/**
+ * A fetcher function for all api interactions.
+ * @param resource Resource must start with / (slash)
+ *
+ * @example
+    try {
+      const response = await fetcher("/auth/login", {
+        method: "POST",
+        body: {
+          email,
+          password
+        },
+      });
+      console.log(response)
+    } catch (err) {
+      console.log(err.info)
+    }
+  };
+ */
+const fetcher = async (resource: string, init?: RequestInit) => {
+  const response = await fetch(`${API_URL}${resource}`, init);
+
+  if (!response.ok) {
+    const error = new Error("Failed to fetch data.");
+    throw error;
+  }
+
+  const result = await response.json();
+  return result;
+};
+
+export default fetcher;
